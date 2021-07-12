b8345ad460f82d8966adbcd757582bf47ccf5d6d
zhhanging <zhhanging@outlook.com>
2021-07-12 12:07:31 +0800

    add trunc

--- lodash/zhhanging-lodash.js
+++ lodash/zhhanging-lodash.js
@@ -1,8 +1,21 @@
 let zhhanging = (function () {
   function trunc(array, size = 1) {
-    
+    let result = [];
+    for (let i = 0; i < array.length; ) {
+      let piece = [];
+      let j = 0;
+      for (j = 0; j < size; j++) {
+        if (!array[i]) break;
+        piece.push(array[i]);
+        i++;
+      }
+      result.push(piece);
+    }
+    return result;
   }
   return {
     trunc: trunc,
   };
 })();
+
+// console.log(zhhanging.trunc(["a", "b", "c", "d", "e"], 3));
